4219a09c1a6cc7962589b20ae8758592dfe8033c
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2020-01-30 03:05:28 -0300

    19.3 Validacion formularios required|min:3

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -9,7 +9,9 @@ class MessagesController extends Controller
     public function store(){
     	request()->validate([
     		'name' => 'required',
-    		'email' => 'required | email'
+    		'email' => 'required|email',
+    		'subject' => 'required',
+    		'content' => 'required|min:3'
     	]);//si falla devuelve la pagina automaticamente
     	return 'Datos validados';
     }
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -9,11 +9,15 @@
 		<input name="name" placeholder="Nombre" ><br>
 		{!! $errors->first('name', '<small>:message</small><br>') !!}
 
-		<input type="email" name="email" placeholder="Email..." value="jmr@mail.com"><br>
+		<input type="text" name="email" placeholder="Email..." value="jmr@mail.com"><br>
 		{!! $errors->first('email', '<small>:message</small><br>') !!}
 
 		<input name="subject" placeholder="Asunto..." value="Asunto de prueba"><br>
+		{!! $errors->first('subject', '<small>:message</small><br>') !!}
+
 		<textarea name="content" placeholder="Mensaje...">Mensaje de prueba</textarea><br>
+		{!! $errors->first('content', '<small>:message</small><br>') !!}
+
 		<button>Enviar</button>
 	</form>
 @endsection
