e6d6676dd227831758245e976dbaa5ebad67031d
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2020-01-30 00:51:19 -0300

    19.2 Validacion formularios $errors->first()

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,13 +6,12 @@
 	<h1>Contact</h1>
 	<form method="POST" action="{{ route('contact') }}">
 		@csrf
-		@if($errors->any())
-			@foreach($errors->all() as $error)
-				<p>{{ $error }}</p>
-			@endforeach
-		@endif
 		<input name="name" placeholder="Nombre" ><br>
+		{!! $errors->first('name', '<small>:message</small><br>') !!}
+
 		<input type="email" name="email" placeholder="Email..." value="jmr@mail.com"><br>
+		{!! $errors->first('email', '<small>:message</small><br>') !!}
+
 		<input name="subject" placeholder="Asunto..." value="Asunto de prueba"><br>
 		<textarea name="content" placeholder="Mensaje...">Mensaje de prueba</textarea><br>
 		<button>Enviar</button>
