c781f28f0b4dd96b0b71d469a4d11566e776a86a
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2020-02-04 01:54:11 -0300

    24.4 Eloquent: Obtener registros de la base de datos create_at->format()

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -7,7 +7,7 @@
 	<ul>
 
 		@forelse($portfolio as $portfolioItem)
-			<li>{{ $portfolioItem->title }} <br><small>{{ $portfolioItem->description }}</small></li><br>
+			<li>{{ $portfolioItem->title }} <br><small>{{ $portfolioItem->description }}</small><br>{{ $portfolioItem->created_at->format('d-m-yy') }} - {{ $portfolioItem->created_at->diffForHumans() }}</li><br>
 		@empty
 			<li>No hay proyectos para mostrar</li>
 		@endforelse
