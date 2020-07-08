9c87f980da3bab7468063378765515f30887f5be
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2020-07-08 10:18:57 -0300

    33. Route Resource

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -19,9 +19,9 @@ class ProjectController extends Controller
             'projects' => Project::latest()->paginate(4)
         ]);// por defecto la paginacion es de 15 items por pagina
     }
-    public function show(Project $id)
+    public function show(Project $project)
     {
-        return view('projects.show', ['project' => $id]);
+        return view('projects.show', ['project' => $project]);
     }
     //metodo que muestra el formulario
     public function create()
@@ -46,19 +46,23 @@ class ProjectController extends Controller
     }
 
     //metodo que muestra formulario con el @method('PATH')
-    public function edit(Project $id)
+    public function edit(Project $project)
     {
-        return view('projects.edit', ['project' => $id]);
+        return view('projects.edit', ['project' => $project]);
     }
     //metodo para procesar formulario de edit()
-    public function update(Project $id, SaveProjectRequest $request)
+    public function update(Project $project, SaveProjectRequest $request)
     {
-        $id->update($request->validated());
-        return redirect()->route('projects.show', $id);
+        /*
+            nombre del parámetro en rutas tiene que ser el mismo que recibe 
+            en el controlador ->parameters(['portfolio' => 'project']) tiene ser project 
+        */
+        $project->update($request->validated());
+        return redirect()->route('projects.show', $project);
     }
-    public function destroy(Project $id)
+    public function destroy(Project $project)
     {
-        $id->delete();
+        $project->delete();
         return redirect()->route('projects.index');//similar a la accion de store()
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,23 @@ App::setLocale('es');
 
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
-
+/*
+    La aplicación va a seguir funcionando normal, solo que en todos los parámetros de las 
+    funciones del controller hay que reemplazar $id por $project porque el resource 
+    tiene ->parameters(['portfolio' => 'project']) si no va a dar error la aplicación
+*/
+Route::resource('portfolio', 'ProjectController')
+    ->parameters(['portfolio' => 'project'])
+    ->names('projects');
+/* 
 Route::get('/portfolio', 'ProjectController@index')->name('projects.index');//devuelve listado
 Route::get('/portfolio/create', 'ProjectController@create')->name('projects.create');//create tiene q estar x arriba de show y store
-
 Route::get('/portfolio/{id}/edit', 'ProjectController@edit')->name('projects.edit');
 Route::patch('/portfolio/{id}', 'ProjectController@update')->name('projects.update');//similar a @show pero cambia tipo PATCH
-
 Route::post('/portfolio', 'ProjectController@store')->name('projects.store');
 Route::get('/portfolio/{id}', 'ProjectController@show')->name('projects.show');//devuelve un projecto x
-
-Route::delete('/portfolio/{id}', 'ProjectController@destroy')->name('projects.destroy');
+Route::delete('/portfolio/{id}', 'ProjectController@destroy')->name('projects.destroy'); 
+*/
 
 Route::view('/contact', 'contact')->name('contact');
 Route::post('/contact', 'MessageController@store')->name('messages.store');
