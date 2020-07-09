73f55e76a97b3f189bb798c3a060cc180f883042
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2020-07-09 16:11:39 -0300

    35.3 Agregando login y registro - directiva @guest de blade

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -4,6 +4,8 @@
 			<li class="{{ setActive('about') }}"><a href="{{route('about')}}">@lang('About')</a></li>
 			<li class="{{ setActive('projects.*') }}"><a href="{{route('projects.index')}}">@lang('Projects')</a></li>
 			<li class="{{ setActive('contact') }}"><a href="{{route('contact')}}">@lang('Contact')</a></li>
-			<li><a href="{{route('login')}}">@lang('Login')</a></li>
+			@guest
+				<li><a href="{{route('login')}}">@lang('Login')</a></li>
+			@endguest	
 		</ul>
 </nav>
