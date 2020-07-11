6759f55b71e5e560e09e9bfb97a86b6efb4b9de3
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2020-07-11 04:17:04 -0300

    35.4 Agregando login y registro - add Logout

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -4,8 +4,18 @@
 			<li class="{{ setActive('about') }}"><a href="{{route('about')}}">@lang('About')</a></li>
 			<li class="{{ setActive('projects.*') }}"><a href="{{route('projects.index')}}">@lang('Projects')</a></li>
 			<li class="{{ setActive('contact') }}"><a href="{{route('contact')}}">@lang('Contact')</a></li>
+
 			@guest
 				<li><a href="{{route('login')}}">@lang('Login')</a></li>
-			@endguest	
+			@else		
+				<li> 
+					<a href="#" onclick="event.preventDefault();
+					document.getElementById('logout-form').submit();">
+					{{ __('Logout') }}</a>
+				</li>			
+			@endguest
 		</ul>
+		<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+			@csrf
+		</form>
 </nav>
