847aedff183eb7eca00150e3b664dc046b8b7a37
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2020-07-12 06:43:05 -0300

    36.3 Proteger rutas con usuario y contrasena - constructor controller middleware except

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -11,7 +11,7 @@ class ProjectController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth')->only('create', 'edit');
+        $this->middleware('auth')->except('index', 'show');
     }
     /**
      * Display a listing of the resource.
