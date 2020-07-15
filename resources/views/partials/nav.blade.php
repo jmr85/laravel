1bd7780b6e7eadb43c8a8d580ac5751ddb473807
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2020-07-15 14:27:30 -0300

    38.1 Diseño con Bootstrap 4 - parte 1 - class nav-bar, nav-item, etc

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,17 +1,42 @@
-<nav>
-		<ul>
-			<li class="{{ setActive('home') }}"><a href="{{route('home')}}">@lang('Home')</a></li>
-			<li class="{{ setActive('about') }}"><a href="{{route('about')}}">@lang('About')</a></li>
-			<li class="{{ setActive('projects.*') }}"><a href="{{route('projects.index')}}">@lang('Projects')</a></li>
-			<li class="{{ setActive('contact') }}"><a href="{{route('contact')}}">@lang('Contact')</a></li>
+<nav class="navbar bg-white shadow-sm">
+		<a class="navbar-brand" href="{{ route('home') }}">
+				{{ config('app.name') }} {{-- lo obtiene del archivo .env --}}
+		</a>
+		<ul class="nav nav-pills"> {{-- ubica horizontalmente los liks, nav-pills es el relleno simlar a un boton --}}
+			<li class="nav-item">
+				<a class="nav-link {{ setActive('home') }}"  href="{{route('home')}}">
+					{{-- nav-link y {{setActive('home')}} es lo mismo que nav-link y active  --}}
+					@lang('Home')
+				</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link {{ setActive('about') }}" href="{{route('about')}}">
+					@lang('About')
+				</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link {{ setActive('projects.*') }}" href="{{route('projects.index')}}">
+					@lang('Projects')
+				</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link {{ setActive('contact') }}" href="{{route('contact')}}">
+					@lang('Contact')
+				</a>
+			</li>
 
 			@guest
-				<li><a href="{{route('login')}}">@lang('Login')</a></li>
+				<li class="nav-item">
+					<a class="nav-link {{ setActive('login') }}"  href="{{route('login')}}">
+						@lang('Login')
+					</a>
+				</li>
 			@else		
-				<li> 
-					<a href="#" onclick="event.preventDefault();
-					document.getElementById('logout-form').submit();">
-					{{ __('Logout') }}</a>
+				<li class="nav-item"> 
+					<a class="nav-link {{ setActive('logout-form') }}" href="#" onclick="event.preventDefault();
+						document.getElementById('logout-form').submit();">
+						{{ __('Logout') }}
+					</a>
 				</li>			
 			@endguest
 		</ul>
