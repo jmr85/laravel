c250275e9ddfd24b467a1ab784a804633041b7c1
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2020-07-17 02:16:19 -0300

    38.3 Diseno con Bootstrap 4 - parte 1 - Flex Bootstrap y div=app layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,17 +2,28 @@
 <html>
 <head>
 	<title>@yield('title', 'Aprendible')</title>
+	{{-- indicamos la escala inicial al momento de cargar la pagina, va a ser 1 o sea 100% --}}
+	{{-- el ancho de la aplicacion sea el mismo ancho del dispositivo --}}
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- CSRF Token para axios -->
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 	<script src="{{ mix('/js/app.js') }}" defer></script>
 </head>
 <body>
-	@include('partials.nav')
+	<div id="app" class="d-flex flex-column h-screen justify-content-between"><!-- lo requiere VueJS -->
+		<header>
+            @include('partials.nav')
+            @include('partials.session-status')
+        </header>
 	
-	@include('partials.session-status')
-
-	@yield('content')
+		<main>
+            @yield('content')
+		</main>
+		
+		<footer class="bg-white text-center text-black-50 py-3 shadow">
+            {{ config('app.name') }} | Copyright @ {{ date('Y') }}
+        </footer>
+	</div>
 </body>
 </html>
-
-
-
